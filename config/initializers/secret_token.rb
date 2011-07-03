fc8e089b00e3c3dd4a8c80e4cc6b837bea6ed680
Alex <lyosha@gmail.com>
2011-07-03 11:50:08 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThePredrink::Application.config.secret_token = '83f8d681190cc1266b2219a4971b4856936d879319c856df05f7f44b93635d1ebdd5d244c0fc542942dee6a808c216e1c9963bd812b8ca600ac5aaeded47f9e9'
